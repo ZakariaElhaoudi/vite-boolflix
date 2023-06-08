b6c1036abb5a1ce30d471e0065259e329b384cca
Zakaria96 <zakarialavoro@gmail.com>
2023-06-08 11:56:29 +0200

    Milestone 2

--- src/components/AppFilmCard.vue
+++ src/components/AppFilmCard.vue
@@ -7,23 +7,30 @@ export default {
     },
     methods: {
         getLanguage(language) {
-            return `fi fi-${language.toLowerCase()}`;
+
+            if (language === 'en') {
+                return 'fi fi-gb'
+            } else if (!language ? `fi fi-${language}` : '${language}') {
+                return `${language}`
+            } else {
+                return `fi fi-${language.toLowerCase()}`;
+            }
         }
-    }
+    },
 }
 </script>
 
 <template>
     <!-- Titolo Titolo Originale Lingua Voto -->
 
-    <div class="col-6 col-md-4 col-lg-3 card my-2 mx-3">
+    <div class="col-6 col-md-4 col-lg-3 card my-2 mx-3 d-flex flex-column justify-content-center align-items-center">
         <h4>Titolo: {{ details.title }}</h4>
         <h4>Titolo Originale: {{ details.original_title }}</h4>
         <!-- <h4>Lingua: {{ details.original_language }}</h4> -->
         <span :class="getLanguage(details.original_language)" class="flag"></span>
         <h4>Voto: {{ details.vote_average }}</h4>
     </div>
-    <div class="col-6 col-md-4 col-lg-3 card my-2 mx-3">
+    <div class="col-6 col-md-4 col-lg-3 card my-2 mx-3 d-flex flex-column justify-content-center align-items-center">
         <h4>Titolo: {{ details.original_name }}</h4>
         <h4>Titolo Originale: {{ details.name }}</h4>
         <span :class="getLanguage(details.original_language)" class="flag"></span>
@@ -31,8 +38,4 @@ export default {
     </div>
 </template>
 
-<style lang="scss" scoped>
-.flag {
-    text-align: center;
-}
-</style>
+<style lang="scss" scoped></style>
