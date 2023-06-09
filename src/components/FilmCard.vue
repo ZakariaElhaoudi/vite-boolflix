c5db86aaa8d785adbe036c42a40e613c8a84b9e8
Zakaria96 <zakarialavoro@gmail.com>
2023-06-09 10:37:20 +0200

    added stars vote

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -18,37 +18,44 @@ export default {
             return Math.floor(vote / 2);
         },
     }
+
 }
 </script>
 
 <template>
     <!-- Titolo Titolo Originale Lingua Voto -->
-    <div class="col-6 col-md-4 col-lg-3 card my-2 mx-3">
-        <img :src="`https://image.tmdb.org/t/p/w342${details.backdrop_path}`" :alt="details.title" :key="details.id">
-    </div>
+    <!-- <div class="col-6 col-md-4 col-lg-3 card my-2 mx-3">
+                                                                                        <img :src="`https://image.tmdb.org/t/p/w342${details.backdrop_path}`" :alt="details.title" :key="details.id">
+                                                                                    </div> -->
 
-    <div v-if="details.id"
-        class="col-6 col-md-4 col-lg-3 card my-2 mx-3 d-flex flex-column justify-content-center align-items-center">
-        <!-- <img :src="`https://image.tmdb.org/t/p/w342${details.backdrop_path}`" :alt="details.title"> -->
-        <h4>Titolo:{{ details.original_name }}</h4>
+<div v-if="details.id"
+    class="col-6 col-md-4 col-lg-3 card my-2 mx-3 d-flex flex-column justify-content-center align-items-center">
+    <img :src="`https://image.tmdb.org/t/p/w342${details.backdrop_path}`" :alt="details.title">
+    <h4>Titolo:{{ details.original_name }}</h4>
         <h4>Titolo Originale: {{ details.original_title }}</h4>
         <!-- <h4>Lingua: {{ details.original_language }}</h4> -->
-        <span :class="getLanguage(details.original_language)" class="flag">{{ details.original_language }}</span>
-        <!-- <h4>Voto: {{ matchFloor(details.vote_average) }}</h4> -->
-        <div v-for="star in 5" :key="star">
-            <font-awesome-icon v-if="star = matchFloor(details.vote_average)" class="fas fa-star" />
-            <font-awesome-icon v-else icon="far fa-star" />
-            <font-awesome-icon icon="fas fa-star" />
+        <span :class="getLanguage(details.original_language)" class="flag"></span>
+        <div class="d-flex flex-row">
+            <div v-for="star in 5" :key="star">
+                <i v-if="star <= matchFloor(details.vote_average)" class="fa-solid fa-star"></i>
+                <i v-else class="fa-regular fa-star"></i>
+            </div>
         </div>
-        <font-awesome-icon icon="fa-solid fa-star" />
     </div>
+
     <div v-else-if="details.id"
         class="col-6 col-md-4 col-lg-3 card my-2 mx-3 d-flex flex-column justify-content-center align-items-center">
         <img :src="`https://image.tmdb.org/t/p/w342${details.backdrop_path}`" :alt="details.name">
         <h4>Titolo: {{ details.original_name }}</h4>
         <h4>Titolo Originale: {{ details.name }}</h4>
         <span :class="getLanguage(details.original_language)" class="flag"></span>
-        <h4>Voto: {{ matchFloor(details.vote_average) }}</h4>
+        <!-- <h4>Voto: {{ matchFloor(details.vote_average) }}</h4> -->
+        <!-- <div class="d-flex flex-row">
+                    <div v-for="star in 5" :key="star">
+                        <i v-if="star <= matchFloor(details.vote_average)" class="fa-solid fa-star"></i>
+                        <i v-else class="fa-regular fa-star"></i>
+                    </div>
+                </div> -->
     </div>
 </template>
 
